b5e9045515702b38b3332f4297d876b2347cc6b4
Shinji KOBAYASHI <skoba@moss.gr.jp>
2016-11-29 17:56:20 +0900

    fixed schema

--- spec/mmlsg_spec.rb
+++ spec/mmlsg_spec.rb
@@ -1,7 +1,7 @@
 describe 'mmlSg:SurgeryModule' do
   let(:validator) { SampleValidator.new 'surgery.xsd' }
 
-  example 'sample instance validation' do
+  xexample 'sample instance validation' do
     expect(validator).to be_valid 'mmlsg_sample.xml'
   end
 end
--- spec/mmlsm_spec.rb
+++ spec/mmlsm_spec.rb
@@ -1,7 +1,7 @@
 describe 'mmlSm:SummaryModule' do
   let(:validator) { SampleValidator.new 'summary.xsd' }
 
-  xexample 'Sample instance vaildator' do
+  example 'Sample instance vaildator' do
     expect(validator).to be_valid 'mmlsm_sample.xml'
   end
 end
